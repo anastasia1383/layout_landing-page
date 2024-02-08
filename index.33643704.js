407fde0cb6c8fa36ac7892d17f9e8dc1242727df
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-02-08 17:20:23 +0100

    make build

---
+++ index.33643704.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".icon--menu"),n=document.querySelector(".icon--close"),t=document.querySelector(".nav__list"),c=document.querySelector(".menu");e.addEventListener("click",function(){c.classList.add("is-open")}),n.addEventListener("click",function(){c.classList.remove("is-open")}),t.addEventListener("click",function(){c.classList.remove("is-open")})});//# sourceMappingURL=index.33643704.js.map
+
+//# sourceMappingURL=index.33643704.js.map
